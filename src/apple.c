8e1c5d75faea156f18dbbaf7ae20a5bb6c10cef0
tanakasbk <tanaka.s.bk@m.titech.ac.jp>
2019-06-18 16:42:31 +0900

    apple

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,36 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+    int result;
+    for(i = 0; i < n; i++){
+        result += (A[i] + x - 1) / x;
+    }
+    return result <= k;
+}
+
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    
+    lb = 0;
+    ub = n;
+    while(ub - lb > 1){
+        int m = (lb + ub) / 2;
+        if(p(m)){
+            ub = m;
+        }
+        else{
+            lb = m;
+        }
+    }
+    
+    printf("%d\n",ub);
+    
+    
+    return 0;
 }
