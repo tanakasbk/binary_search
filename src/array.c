8607d72b59d4cdc0e706103bb1feee6eef08423c
tanakasbk <51943805+tanakasbk@users.noreply.github.com>
2019-06-18 16:13:23 +0900

    Update array.c

--- src/array.c
+++ src/array.c
@@ -6,13 +6,21 @@ int A[100000];
 
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    
+    lb = -1;
+    ub = n;
+    while(ub - lb > 1){
+        int m = (lb + ub) / 2;
+        if(A[m] >= k) ub = m;
+        else lb = m;
+    }
+    
+    printf("%d\n", ub);
+    
+    return 0;
 }
