d9e8be092fa886f841c6b8603c61e79ffb25475e
田中咲 <saki@tanakasakinoMacBook-Air.local>
2019-06-20 21:35:11 +0900

    apple

--- src/apple.c
+++ src/apple.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 
 int n;
 int k;
 int A[100000];
 
 int p(int x){
-    int result;
+    int i, result = 0;
     for(i = 0; i < n; i++){
-        result += (A[i] + x - 1) / x;
+        result += ceil((double)A[i] / x);
     }
     return result <= k;
 }
 
 
 int main(){
-    int i, lb, ub;
+    int i, lb, ub, maxApple = 0;
     scanf("%d%d", &n, &k);
     for(i = 0; i < n; i++){
         scanf("%d", &A[i]);
+        if(A[i] > maxApple)maxApple = A[i];
     }
     
     lb = 0;
-    ub = n;
+    ub = maxApple;
     while(ub - lb > 1){
         int m = (lb + ub) / 2;
         if(p(m)){
